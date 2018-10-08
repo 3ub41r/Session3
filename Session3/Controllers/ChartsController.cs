68bc9ae7a5c66afddf522add89d633b63ede4d2d
zubair <zubairabuhasan@gmail.com>
2018-10-08 08:03:48 +0800

    Initial commit

---
+++ Session3/Controllers/ChartsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session3.Controllers
+{
+    public class ChartsController : Controller
+    {
+        // GET: Charts
+        public ActionResult Index()
+        {
+            var countries = new ChartViewModel
+            {
+                CountryVisitses = new List<CountryVisits>
+                {
+                    new CountryVisits
+                    {
+                       country = "Malaysia",
+                       visits = 3200
+                    },
+                    new CountryVisits
+                    {
+                        country = "Indonesia",
+                        visits = 7200
+                    },
+                    new CountryVisits
+                    {
+                        country = "China",
+                        visits = 3300
+                    }
+                }
+            };
+            return View(countries);
+        }
+    }
+
+    public class ChartViewModel
+    {
+        public List<CountryVisits> CountryVisitses { get; set; }
+    }
+
+    public class CountryVisits
+    {
+        public string country { get; set; }
+        public int visits { get; set; }
+    }
+}
